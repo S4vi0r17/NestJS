5a07d25b898c6064492cf0efdc38e5cdd39d4abe
S4vi0r <gbp17@outlook.com>
2024-07-03 16:56:44 -0500

    chore: Update getCarById endpoint in CarsController to use ParseIntPipe

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -11,7 +11,7 @@ export class CarsController {
   }
 
   @Get(':carId')
-  getCarById(@Param('carId') carId: string) {
-    return this.carsService.getCarById(+carId);
+  getCarById(@Param('carId', ParseIntPipe) carId: number) {
+    return this.carsService.getCarById(carId);
   }
 }
