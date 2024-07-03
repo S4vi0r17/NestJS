07930dfd2b6905614048980c7e095bd7a4b2bcf4
S4vi0r <gbp17@outlook.com>
2024-07-03 16:36:46 -0500

    feat: Add endpoint to get all cars and get car by ID in CarsController

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  private cars = ['car1', 'car2', 'car3'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':carId')
+  getCarById(@Param('carId') carId: string) {
+    return this.cars[+carId];
+  }
+}
