1d0465f33725532ed6948f71917e95e7b510549d
S4vi0r <gbp17@outlook.com>
2024-07-04 12:26:51 -0500

    feat: Add global validation pipe to main.ts for whitelisting and forbidding non-whitelisted properties

--- 02-car-dealership/src/main.ts
+++ 02-car-dealership/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove non-whitelisted properties
+      forbidNonWhitelisted: true, // throw an error when non-whitelisted properties are present
+      // transform: true, // transform payloads to DTO instances
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
