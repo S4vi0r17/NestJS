8e214c906771a986f5adb14c8e30582465fda7b3
S4vi0r <gbp17@outlook.com>
2024-07-05 00:27:28 -0500

    feat: Add CarsModule and BrandsModule to SeedModule imports

--- 02-car-dealership/src/seed/seed.service.ts
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from 'src/cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brand.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
   populateDatabase() {
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
+
     return 'Seed data created successfully!';
   }
 }
