09502637873d991ea8cca6f48e43e988e11a24e3
S4vi0r <gbp17@outlook.com>
2024-07-07 16:38:10 -0500

    chore: Add PokemonModule to SeedModule imports

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(private readonly pokemonService: PokemonService) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=5',
     );
 
     data.results.forEach(({ name, url }) => {
@@ -16,6 +19,8 @@ export class SeedService {
       const number = +segments[segments.length - 2];
 
       console.log({ number, name });
+
+      this.pokemonService.create({ number, name });
     });
 
     return data.results;
