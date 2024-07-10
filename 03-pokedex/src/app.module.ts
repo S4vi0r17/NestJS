6ea49a9db6f8ec607be78322e332eae446a3c1d5
S4vi0r <gbp17@outlook.com>
2024-07-09 21:54:36 -0500

    refactor: Update .gitignore and package.json to include .env file and add @nestjs/config dependency

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -5,14 +5,19 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration global
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -24,4 +29,8 @@ import { SeedModule } from './seed/seed.module';
   providers: [],
   exports: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
