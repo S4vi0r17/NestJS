76d012190a0933ee609d541cb1d664436f3e9464
S4vi0r <gbp17@outlook.com>
2024-07-14 15:13:26 -0500

    refactor: Update CreateProductDto with validation decorators and optional fields

--- 04-e-comerce/src/products/dto/create-product.dto.ts
+++ 04-e-comerce/src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'woman', 'kid', 'unisex'])
+  gender: string;
+}
