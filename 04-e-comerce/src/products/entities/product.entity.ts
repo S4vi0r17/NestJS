2ebaf86fcafae85bf7e73bc0e183b4f3711fd6f4
S4vi0r <gbp17@outlook.com>
2024-07-13 23:27:03 -0500

    Solucionar de creacción de base de datos en docker-compose.yml

--- 04-e-comerce/src/products/entities/product.entity.ts
+++ 04-e-comerce/src/products/entities/product.entity.ts
@@ -1,1 +1,14 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column('text', { unique: true })
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: string;
+}
