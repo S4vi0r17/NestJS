edb2a20e27dc11f8c908da368c04ffa819310302
S4vi0r <gbp17@outlook.com>
2024-07-13 22:20:42 -0500

    refactor: Update .dockerignore, .gitignore, Dockerfile, and docker-compose.prod.yaml for Docker containerization

---
+++ 04-e-comerce/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // This will automatically load all entities in the entities folder
+      synchronize: true, // This will automatically create database tables based on the entities
+    }),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
