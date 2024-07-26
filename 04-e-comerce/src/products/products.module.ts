c8cbd5aaf6c53d7b8fc6402fbb016a2b3cbe31a8
S4vi0r <gbp17@outlook.com>
2024-07-25 22:39:11 -0500

    refactor: Add ProductImage entity to ProductsModule imports

--- 04-e-comerce/src/products/products.module.ts
+++ 04-e-comerce/src/products/products.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
 })
 export class ProductsModule {}
