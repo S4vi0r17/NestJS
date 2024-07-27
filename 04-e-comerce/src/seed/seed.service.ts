8486c778f93b5fee057b7f1348550afb073a40ff
S4vi0r <gbp17@outlook.com>
2024-07-26 21:32:56 -0500

    refactor: Add initial product data seeding in SeedService

--- 04-e-comerce/src/seed/seed.service.ts
+++ 04-e-comerce/src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,5 +13,15 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = [];
+
+    products.forEach(async (product) => {
+      insertPromises.push(await this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
   }
 }
