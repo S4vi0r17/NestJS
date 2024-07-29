b468faeeff046d62c5c37073cf85984f6af97513
S4vi0r <gbp17@outlook.com>
2024-07-28 20:29:07 -0500

    refactor: Add TypeOrmModule to AuthModule and import User entity

--- 04-e-comerce/src/auth/entities/user.entity.ts
+++ 04-e-comerce/src/auth/entities/user.entity.ts
@@ -1,15 +1,22 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('text', { unique: true })
   email: string;
 
+  @Column('text')
   password: string;
 
+  @Column('text')
   fullName: string;
 
+  @Column('boolean', { default: true })
   isActive: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
 }
