201efddd3ba9c9afe329f1675711c907e3df81f4
S4vi0r <gbp17@outlook.com>
2024-07-28 21:10:40 -0500

    refactor: Add login endpoint to AuthController and implement login functionality

--- 04-e-comerce/src/auth/auth.service.ts
+++ 04-e-comerce/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,6 +11,7 @@ import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +34,28 @@ export class AuthService {
     } catch (error) {
       this.handleDBError(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      // select: { email: true, password: true },
+      select: ['email', 'password'],
+    });
+
+    if (!user) {
+      throw new UnauthorizedException(`User with email ${email} not found`);
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return user;
   }
 
   private handleDBError(error: any): never {
