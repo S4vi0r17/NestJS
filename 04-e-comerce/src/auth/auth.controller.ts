3904bc820adac4a936f3f687090b5a8439e3a204
S4vi0r <gbp17@outlook.com>
2024-07-30 18:34:04 -0500

    refactor: Add GetUser decorator for retrieving user from request

--- 04-e-comerce/src/auth/auth.controller.ts
+++ 04-e-comerce/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -20,9 +22,11 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(@Req() req: Express.Request, @GetUser() user: User) {
+    // console.log(req);
+    // console.log(user);
     return {
-      message: 'This is a private route',
+      message: user,
     };
   }
 }
