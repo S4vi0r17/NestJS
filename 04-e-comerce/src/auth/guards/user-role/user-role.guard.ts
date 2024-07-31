f2cb5f04ae553285278c23cdf12fb0570a0ef8bd
S4vi0r <gbp17@outlook.com>
2024-07-30 19:13:42 -0500

    refactor: Update UserRoleGuard to handle role-based authorization
    The UserRoleGuard in the 04-e-comerce project has been updated to handle role-based authorization. This update allows the guard to check if the user has the required roles specified in the 'roles' metadata. If the user does not have the required roles, a ForbiddenException is thrown with a detailed error message.
    
    This change improves the security and access control of the application by ensuring that only users with the necessary roles can access certain routes or perform specific actions.
    
    Ref: #123

--- 04-e-comerce/src/auth/guards/user-role/user-role.guard.ts
+++ 04-e-comerce/src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -10,7 +17,28 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    console.log(roles);
-    return true;
+
+    if (!roles) return true;
+
+    if (roles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    // console.log('User role:', user.roles);
+
+    for (const role of roles) {
+      if (user.roles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.email} does not have the required roles: ${roles.join(', ')}`,
+    );
   }
 }
