e7a3fb46af1e6382294f6a5808aba4d87b3dc015
S4v!0r <gbp17@outlook.com>
2025-05-06 00:15:41 -0500

    feat: enhance product and seed controllers with role-based access control

--- 04-nest-auth-orm/src/seed/seed.controller.ts
+++ 04-nest-auth-orm/src/seed/seed.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Post()
+  // @Auth()
+  @Auth(ValidRoles.admin)
   executeSeed() {
     return this.seedService.executeSeed();
   }
--- 04-nest-auth-orm/src/seed/seed.module.ts
+++ 04-nest-auth-orm/src/seed/seed.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { ProductsModule } from 'src/products/products.module';
 import { SeedController } from './seed.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule],
   controllers: [SeedController],
   providers: [SeedService],
 })
